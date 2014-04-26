edff42cba77465c4f2ee3efe4bfc7ee249117fdf
Bob Mottram <bob@robotics.uk.to>
2014-04-26 18:49:51 +0100

    Create manpage

--- makeJargon.py
+++ makeJargon.py
@@ -33,5 +33,27 @@ def jargonGetEntries(entriesDir):
                 entries.append(entry)
     return entries
 
+def jargonToManpage(manpageFilename, entries):
+    if not os.path.isdir("man"):
+        os.system("mkdir man")
+
+    fp = open(manpageFilename,'w')
+
+    fp.write(".TH \"The Jargon File\" 1 \"April 26, 2014\" \"\" \"The Jargon File\"\n\n")
+
+    for entry in entries:
+        title = entry[0]
+        text = entry[1]
+        fp.write(".SH " + title + "\n")
+        fp.write(text + "\n\n")
+
+    fp.close()
+
+    os.system("gzip " + manpageFilename)
+    print ""
+    print "manpage can be installed with the command:"
+    print "sudo install -m 644 " + manpageFilename + ".gz /usr/local/share/man/man1"
+
 if __name__ == "__main__":
     entries = jargonGetEntries('entries')
+    jargonToManpage("man/jargon.1", entries)
